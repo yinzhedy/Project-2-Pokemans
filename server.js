066424a2ffbe0f02ec2dfa3d387afe878658aa99
Jack Ault <henlowgg@outlook.com>
2021-10-25 02:23:17 -0400

    user routes written out almost fully

--- server.js
+++ server.js
@@ -23,6 +23,79 @@ app.use(session({
     }
 }));
 
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
+
+var hbsContent = {userName: '', loggedIn: false, title: "You are not logged in today", body: "Hello World"};
+
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }
+};
+
+// route for user signup
+
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+app.route('/signup')
+    .get((req, res) => {
+        res.render('signup', hbsContent);
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            password: req.body.password
+        })
+            .then(user => {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            })
+            .catch(err => {
+                res.redirect('/signup');
+            });
+    });
+
+// route for user Login
+app.route('/login')
+    .get((req, res) => {
+        res.render('login', hbsContent);
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+        var password = req.body.password;
+        
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+
 const db = require("./app/models");
 db.sequelize.sync();
 // drop the table if it already exists
