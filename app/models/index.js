f5d303b0682c1e0803086b22df0b3f190a23c000
Jack Ault <henlowgg@outlook.com>
2021-10-26 21:14:34 -0400

    server is running, now to get api info to work

--- app/models/index.js
+++ app/models/index.js
@@ -1,24 +1,24 @@
-const dbConfig = require("../config/db.config.js");
+// const dbConfig = require("../config/db.config.js");
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: 0,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle
+//   }
+// });
 
-const db = {};
+// const db = {};
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
-db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+// db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 
-module.exports = db;
+// module.exports = db;
--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,18 +1,18 @@
-module.exports = (sequelize, Sequelize) => {
-    const Tutorial = sequelize.define("tutorial", {
-      title: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      published: {
-        type: Sequelize.BOOLEAN
-      }
-    });
+// module.exports = (sequelize, Sequelize) => {
+//     const Tutorial = sequelize.define("tutorial", {
+//       title: {
+//         type: Sequelize.STRING
+//       },
+//       description: {
+//         type: Sequelize.STRING
+//       },
+//       published: {
+//         type: Sequelize.BOOLEAN
+//       }
+//     });
   
-    return Tutorial;
-  };
+//     return Tutorial;
+//   };
 
 
 //   SHORT CUTS TO USE FOR MODELS
