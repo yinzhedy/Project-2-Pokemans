fdd3a6870ca479f0d2193ebc5519b7a418697653
Jack Ault <henlowgg@outlook.com>
2021-10-27 18:09:48 -0400

    hbs

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 
 // not using this yet
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
